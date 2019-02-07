a3ead60ca3e0083f4cbc03ad4ee1ce16902d2158
James Carr <carr5618@gmail.com>
2019-02-06 19:10:56 -0500

    update comments

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,6 +1,6 @@
 <?php
   /*
-    ** Heart of the site.
+    ** Heart of the site
     ** Takes in the url and sees where to send
     ** From the URL it sets the controller/method/parameters that are involed
     ** Requires where we need to go
@@ -38,7 +38,7 @@
       $this->params = $url ? array_values($url) : [];
 
       // Call the callback with array of params
-      call_user_func_array([$this->currentController, $this->currentMethod], $this->params); 
+      call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
     public function getUrl(){
