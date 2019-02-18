539a018f12a86ec57ba9a0f88e29fef9155b7e9e
James Carr <carr5618@gmail.com>
2019-02-18 17:09:01 -0500

    Only Working Features

--- app/views/dashboards/index.php
+++ app/views/dashboards/index.php
@@ -27,42 +27,21 @@
       </nav>
       <div class="hidden" id="Investments-hidden">
         <a class="sub-link" href="<?php echo URLROOT;?>/dashboards/investment_general">General</a>
-        <a class="sub-link disable" href="">Strategy</a>
-        <a class="sub-link disable" href="">Calculator</a>
-        <a class="sub-link disable" href="">Holdings</a>
-        <a class="sub-link disable" href="">Futures</a>
       </div>
 
       <div class="hidden" id="Developing-hidden">
         <a class="sub-link" href="<?php echo URLROOT;?>/dashboards/developing_general">General</a>
-        <a class="sub-link disable" href="">Design tips</a>
-        <a class="sub-link disable" href="">UI/UX</a>
-        <a class="sub-link disable" href="">Back-end</a>
-        <a class="sub-link disable" href="">front-end</a>
-        <a class="sub-link disable" href="">Developing Security</a>
-        <a class="sub-link disable" href="">Tips</a>
       </div>
 
       <div class="hidden" id="Tasks-hidden">
-        <a class="sub-link disable" href="">List</a>
-        <a class="sub-link disable" href="">Stopwatch</a>
-        <a class="sub-link disable" href="">Reminders</a>
       </div>
 
       <div class="hidden" id="Personal-hidden">
-        <a class="sub-link disable" href="">Expenses/Income</a>
-        <a class="sub-link disable" href="">Passwords</a>
-        <a class="sub-link disable" href="">Nutrition</a>
-        <a class="sub-link disable" href="">Resume</a>
-        <a class="sub-link disable" href="">Goals</a>
-        <a id="logout"href="<?php echo URLROOT?>/dashboards/logout"> Logout</a>
       </div>
 
       <div class="hidden" id="Settings-hidden">
         <div id="mobile-settings-header"><?php echo $data['username'];?></div>
         <a class="sub-link disable" id="logout"href="<?php echo URLROOT?>/dashboards/logout"> Logout</a>
-        <a class="sub-link disable" href="">Change Username</a>
-        <a class="sub-link disable" href="">Change Password</a>
       </div>
 
 
